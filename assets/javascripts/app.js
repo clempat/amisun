5eb625bfc924889ac3bab11279422cc709ab55c0
clempat <clement.patout@gmail.com>
2013-03-22 16:21:20 +0100

    correct notify bro

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -43,6 +43,13 @@ var $buoop = {}
 $buoop.ol = window.onload;
 
 $(window).load(function() {
+
+    try {if ($buoop.ol) $buoop.ol();}catch (e) {}
+    var e = document.createElement("script");
+    e.setAttribute("type", "text/javascript");
+    e.setAttribute("src", "http://browser-update.org/update.js");
+    document.body.appendChild(e);
+    
     $("#reference").orbit({captions: true,  bullets: true});
     $("#contactForm").validate({
         rules: {
@@ -72,11 +79,7 @@ $(window).load(function() {
         }
     });
 
-    try {if ($buoop.ol) $buoop.ol();}catch (e) {}
-    var e = document.createElement("script");
-    e.setAttribute("type", "text/javascript");
-    e.setAttribute("src", "http://browser-update.org/update.js");
-    document.body.appendChild(e);
+
 
 
 
