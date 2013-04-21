9d420965a0674172b9bcf9aa567c0d21f810e34a
lobu_cpa <clement.patout@ecabo.de>
2013-04-21 19:34:13 +0200

    update nettoyage add rotative

--- application/views/pages/nettoyage.php
+++ application/views/pages/nettoyage.php
@@ -13,26 +13,38 @@
 <div class="twelve columns text-justify" style="margin-top: 20px">
     <?php echo img(array('src' => 'assets/images/nettoyage.png', 'class' => 'five columns')); ?>
     <p>
-        Un nettoyage à l'eau pure est nécessaire 1 à 2 fois par an pour éviter une perte de production et optimiser votre rendement. En effet, le dépôt de salissures (poussières, pollen, fientes, feuilles, pollution agricole, urbaine ou industrielle…) sur la vitre du panneau solaire entraine une diminution du rendement de l’installation et par conséquence une perte financière pour l’exploitant. L’encrassement des panneaux peut entrainer <strong>une perte de production de 3% à 8% pouvant aller jusqu'à 15% dans certains cas.</strong>
+        Un nettoyage à l'eau pure est nécessaire 1 à 2 fois par an pour éviter une perte de production et optimiser
+        votre rendement. En effet, le dépôt de salissures (poussières, pollen, fientes, feuilles, pollution agricole,
+        urbaine ou industrielle…) sur la vitre du panneau solaire entraine une diminution du rendement de l’installation
+        et par conséquence une perte financière pour l’exploitant. L’encrassement des panneaux peut entrainer <strong>une
+            perte de production de 3% à 8% pouvant aller jusqu'à 15% dans certains cas.</strong>
+    </p>
+    <?php echo img(array('src' => 'assets/images/rotative.jpg', 'class' => 'four columns', 'style'=>'float:right;')); ?>
+    <p>
+        En réponse aux besoins de nos clients, notre procédé permet <strong>un nettoyage de qualité</strong> sur des
+        toitures en hauteur, sur des panneaux en façade ou au sol, tout en assurant sécurité et productivité.
     </p>
 
     <p>
-        En réponse aux besoins de nos clients, notre procédé permet <strong>un nettoyage de qualité</strong> sur des toitures en hauteur, sur des panneaux en façade ou au sol, tout en assurant sécurité et productivité.
-    </p>
-    <p>
-        Nous disposons des garanties techniques et assurances spécifiques lors de nos interventions (CACES, travail en hauteur, habilitation électrique…), nous vous garantissons <strong>un sérieux et un professionnalisme.</strong>
+        Nous disposons des garanties techniques et assurances spécifiques lors de nos interventions (CACES, travail en
+        hauteur, habilitation électrique…), nous vous garantissons <strong>un sérieux et un professionnalisme.</strong>
     </p>
     <?php echo img(array('src' => 'assets/images/nettoyage2.png', 'class' => 'six columns right', 'style' => 'float:right;')); ?>
     <p>
-        Les panneaux sont nettoyés avec une machine industrielle : une <strong>brosse</strong> et de l'<strong>eau pure</strong> obtenue par osmose inverse. L'appareil pulvérise des microparticules d'eau évitant tout choc thermique et permettant une faible consommation d'eau.
+        Les panneaux sont nettoyés avec une machine industrielle : une <strong>brosse</strong> et de l'<strong>eau
+            pure</strong> obtenue par osmose inverse. L'appareil pulvérise des microparticules d'eau évitant tout choc
+        thermique et permettant une faible consommation d'eau.
     </p>
 
     <p>
-        L'eau déminéralisée se transforme en un vrai détergent capable de dissoudre toutes impuretés. Il est ainsi garanti que chaque pollution peut être éliminée avec efficacité.
+        L'eau déminéralisée se transforme en un vrai détergent capable de dissoudre toutes impuretés. Il est ainsi
+        garanti que chaque pollution peut être éliminée avec efficacité.
     </p>
     <?php echo img(array('src' => 'assets/images/nettoyage3.png', 'class' => 'two columns')); ?>
     <p>
-        L'eau pure confère un pouvoir déperlant, une excellente capacité de mouillage et garantit l'absence de trace, même en cas d'exposition directe au soleil. Les charges statiques sont réduites au minimum et par conséquent les nouvelles impuretés adhérent moins rapidement.
+        L'eau pure confère un pouvoir déperlant, une excellente capacité de mouillage et garantit l'absence de trace,
+        même en cas d'exposition directe au soleil. Les charges statiques sont réduites au minimum et par conséquent les
+        nouvelles impuretés adhérent moins rapidement.
     </p>
 
     <p>
