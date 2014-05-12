ef6cbda8e126f04c6324462524a51fe72165b935
clementpatout <clement@patataweb.com>
2014-05-12 22:04:23 +0200

    add pictures

---
+++ config/application/config/email.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: clementpatout
+ * Date: 08.02.13
+ * Time: 01:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+$config['protocol']="sendmail";
+$config['mailtype'] = "html";
