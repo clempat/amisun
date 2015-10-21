53a20599d603c5265d577dfe081e079226991eed
Clément Patout <clement.patout@gmail.com>
2015-10-21 20:32:57 +0200

    Add pvcycle - png update 2

--- application/views/sections/head.php
+++ application/views/sections/head.php
@@ -131,7 +131,7 @@
           <strong> Maintenance - Nettoyage - Dépannage - Installation </strong>
         </p>
         <a href="http://www.qualit-enr.org/amisun/" target="_blank"><?php echo img(array('src' => 'assets/images/qualipv.png', 'height' => '88px', 'class' => '')); ?></a>
-        <a href="http://france.pvcycle.org/" target="_blank"><?php echo img(array('src' => 'assets/images/pvcycle.png', 'height' => '88px', 'class' => '')); ?></a>
+        <a href="http://france.pvcycle.org/" target="_blank"><?php echo img(array('src' => 'assets/images/pvcycle2.png', 'height' => '88px', 'class' => '')); ?></a>
         </div>
     </div>
     <div class="ribbon-wrapper-green"><div class="ribbon-green"><?php echo anchor("contact", "Contact") ?></div></div>
